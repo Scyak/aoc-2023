d106cc182bf5fe852e914a9ad8b8d838e829d17f
Jay Sommer <itsjaysommer@gmail.com>
2023-12-14 23:12:28 +0100

    Solve day 14

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,3 +1,146 @@
 pub fn run_day() {
-    println!("Hello, world!");
+    let input_str = include_str!("input.txt");
+    let input = parse_input(input_str);
+
+    println!(
+        "Day 13 Part 1: Sum of adjusted reflection lines is {}",
+        part_one(&input)
+    );
+    println!(
+        "Day 13 Part 2: With smudges fixed, sum is {}",
+        part_two(&input)
+    );
+}
+
+fn parse_input(input_str: &str) -> Vec<Vec<Vec<char>>> {
+    input_str
+        .split("\n\n")
+        .map(|block| block.lines().map(|line| line.chars().collect()).collect())
+        .collect()
+}
+
+fn part_one(input: &Vec<Vec<Vec<char>>>) -> usize {
+    input.iter().map(|map| find_reflection(map, false, 0).expect("No reflection found in part 1!")).sum()
+}
+
+fn part_two(input: &Vec<Vec<Vec<char>>>) -> usize {
+    // save maps with original reflections to check against later
+    let input_with_reflections: Vec<(Vec<Vec<char>>, usize)> = input.iter().map(|map| (map.to_owned(), find_reflection(map, false, 0).expect("No reflection found in part 1!"))).collect();
+
+    let mut reflection_sum = 0;
+    for (map, reflection_line) in input_with_reflections {
+        let mut success = false;
+        // try out each coordinate for fixing smudge
+        for row in 0..map.len() {
+            for col in 0..map[row].len() {
+                let mut new_map = map.clone();
+                new_map[row][col] = match new_map[row][col] {
+                    '.' => '#',
+                    '#' => '.',
+                    _ => panic!("Unknown symbol!")
+                };
+                // check if there is now a new reflection line
+                match find_reflection(&new_map, true, reflection_line) {
+                    Some(result) => {
+                        success = true;
+                        reflection_sum += result;
+                        break;
+                    },
+                    None => {}
+                }
+            }
+            if success {
+                break;
+            }
+        }
+        if !success {
+            let map_str = map.iter().map(|row| row.iter().collect::<String>() + "\n").collect::<String>();
+            panic!("Couldn't fix smudge for map:\n{map_str}");
+        }
+    }
+
+    reflection_sum
+}
+
+/// finds reflection in map, either horizontally or vertically
+/// returns line nr after reflection for vertical, line nr after reflection * 100 for horizontal
+fn find_reflection(map: &Vec<Vec<char>>, part2: bool, cannot_be: usize) -> Option<usize> {
+    // check horizontally
+    // check for duplicated lines, then verify whether it's a complete reflection for found pairs
+    let horizontal_matches: Vec<usize> = map
+        .windows(2)
+        .enumerate()
+        .filter(|(idx, pair)| pair[0] == pair[1] && verify_reflection(*idx+1, &map))
+        .map(|(idx, _)| idx+1)
+        .collect();
+
+    if !horizontal_matches.is_empty() {
+        // check each found reflection line against the line it's not allowed to be for part 2
+        // if part2 is false this will simply return the first (and presumably only) reflection line found
+        for h_match in horizontal_matches {
+            if !part2 || (h_match * 100) != cannot_be {
+                return Some(h_match * 100);
+            }
+        }
+    }
+
+    // check vertically
+    // transpose map and repeat horizontal process
+    let map = transpose(map.to_vec());
+
+    let vertical_matches: Vec<usize> = map
+        .windows(2)
+        .enumerate()
+        .filter(|(idx, pair)| pair[0] == pair[1] && verify_reflection(*idx+1, &map))
+        .map(|(idx, _)| idx+1)
+        .collect();
+
+    if !vertical_matches.is_empty() {
+        // check each found reflection line against the line it's not allowed to be for part 2
+        // if part2 is false this will simply return the first (and presumably only) reflection line found
+        for v_match in vertical_matches {
+            if !part2 || v_match != cannot_be {
+                return Some(v_match);
+            }
+        }
+    }
+
+    None
+}
+
+fn verify_reflection(index: usize, map: &Vec<Vec<char>>) -> bool {
+    for i in 0..map.len() {
+        if (index as i32 - 1 - i as i32) < 0 {
+            // reached top end of map
+            return true;
+        }
+
+        // compare two lines, moving upwards and downwards from the initial pair
+        match (map.get(index + i), map.get(index - 1 - i)) {
+            (_, None) => return true, // reached end of map without finding line that isn't reflected
+            (None, _) => return true, // reached end of map without finding line that isn't reflected
+            (Some(row1), Some(row2)) => {
+                if row1 != row2 {
+                    // rows don't match, not a true reflection
+                    return false;
+                }
+            }
+        }
+    }
+    
+    // made it all the way through the map without finding a line that isn't reflected
+    true
+}
+
+/// transposes 2D char vec
+fn transpose(map: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut transposed_map: Vec<Vec<char>> = (0..map[0].len()).map(|_| vec![]).collect();
+
+    for line in map {
+        for (item, transposed_row) in line.into_iter().zip(&mut transposed_map) {
+            transposed_row.push(item);
+        }
+    }
+
+    transposed_map
 }
--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,3 +1,132 @@
+use std::collections::HashMap;
+
 pub fn run_day() {
-    println!("Hello, world!");
+    let input_str = include_str!("input.txt");
+    let mut input = parse_input(input_str);
+    println!(
+        "Day 14 Part 1: Total load after tilting north is {}",
+        part_one(&mut input)
+    );
+    println!(
+        "Day 14 Part 2: After 1000000000 spin cycles, load is {}",
+        part_two(&input)
+    );
+
+    println!();
+}
+
+fn parse_input(input_str: &str) -> Vec<Vec<char>> {
+    input_str
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect()
+}
+
+fn part_one(platform: &mut Vec<Vec<char>>) -> usize {
+    // tilt north
+    for row_idx in 0..platform.len() {
+        for col_idx in 0..platform[row_idx].len() {
+            if platform[row_idx][col_idx] == 'O' {
+                move_north(platform, row_idx, col_idx);
+            }
+        }
+    }
+
+    calculate_load(platform)
+}
+
+fn part_two(input: &Vec<Vec<char>>) -> usize {
+    let mut platform = input.to_owned();
+    let mut encountered_patterns = HashMap::new();
+
+    let total_cycles = 1000000000;
+    let mut cycle = 0;
+    let mut loop_found = false;
+
+    while cycle < total_cycles {
+        platform = do_spin_cycle(platform.clone());
+
+        if encountered_patterns.contains_key(&platform) && !loop_found {
+            // found loop!
+            let loop_length = cycle - encountered_patterns[&platform];
+            while cycle + loop_length < total_cycles {
+                cycle += loop_length;
+            }
+            loop_found = true;
+        } else if !loop_found {
+            encountered_patterns.insert(platform.clone(), cycle);
+        }
+
+        cycle += 1;
+    }
+
+    calculate_load(&platform)
+}
+
+fn move_north(platform: &mut Vec<Vec<char>>, row: usize, col: usize) {
+    for new_row in (0..row).rev() {
+        if platform[new_row][col] != '.' {
+            platform[new_row + 1][col] = platform[row][col];
+
+            if (new_row + 1) != row {
+                platform[row][col] = '.';
+            }
+            return;
+        }
+    }
+
+    // if no replacement was done in the loop, rock must end up in row 0
+    platform[0][col] = platform[row][col];
+    if 0 != row {
+        platform[row][col] = '.';
+    }
+}
+
+// why implement tilting west, south and east if you can simply rotate the platform clockwise and tilt north again
+fn rotate_platform(platform: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut rotated_platform: Vec<Vec<char>> = (0..platform[0].len()).map(|_| vec![]).collect();
+
+    for row in platform.iter().rev() {
+        for (col_idx, rock) in row.iter().enumerate() {
+            rotated_platform[col_idx].push(*rock);
+        }
+    }
+
+    rotated_platform
+}
+
+fn do_spin_cycle(platform: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut new_platform = platform.to_vec();
+
+    for _ in 0..4 {
+        // tilt north
+        for row_idx in 0..new_platform.len() {
+            for col_idx in 0..new_platform[row_idx].len() {
+                if new_platform[row_idx][col_idx] == 'O' {
+                    move_north(&mut new_platform, row_idx, col_idx);
+                }
+            }
+        }
+
+        // rotate platform so next face is north
+        new_platform = rotate_platform(&new_platform);
+    }
+
+    new_platform
+}
+
+fn calculate_load(platform: &Vec<Vec<char>>) -> usize {
+    platform
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(load, row)| {
+            row.iter()
+                .map(|rock| match rock {
+                    'O' => load + 1,
+                    _ => 0,
+                })
+                .sum::<usize>()
+        })
+        .sum::<usize>()
 }
